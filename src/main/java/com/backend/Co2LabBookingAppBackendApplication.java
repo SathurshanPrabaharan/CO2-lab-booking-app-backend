43fbaf095d31313d07c5638838d90a9dc125a939
sathurshanath <sathurshanath2021@gmail.com>
2024-03-29 11:31:09 +0530

    Initial congiguration

---
+++ src/main/java/com/backend/Co2LabBookingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2LabBookingAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Co2LabBookingAppBackendApplication.class, args);
+	}
+
+}
