81af8c445677087deccecf2a8dbb68aff08b9148
darship19 <darshika197@gmail.com>
2024-04-06 11:46:46 +0530

    get all inventory and get inventory by id

--- inventory-service/src/main/java/com/inventoryservice/Controllers/InventoryController.java
+++ inventory-service/src/main/java/com/inventoryservice/Controllers/InventoryController.java
@@ -1,19 +1,22 @@
 package com.inventoryservice.Controllers;
 
 import com.inventoryservice.Models.Inventory;
+import com.inventoryservice.Repositories.InventoryRepository;
 import com.inventoryservice.Services.InventoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 
 public class InventoryController {
 
     private InventoryService inventoryService;
+    private InventoryRepository inventoryRepository;
+
 
     public InventoryController(InventoryService inventoryService) {
         super();
@@ -23,6 +26,18 @@ public class InventoryController {
     @PostMapping("/api/v1/inventories")
     public ResponseEntity<Inventory> saveEmployee(@RequestBody Inventory inventory){
         return new ResponseEntity<Inventory>(inventoryService.saveInventory(inventory), HttpStatus.CREATED);
+    }
+    @RequestMapping("/api/v1/inventories")
+    public List<Inventory> getAllInventory(){
+        return inventoryRepository.findAll();
+    }
+
+
+    @GetMapping("/api/v1/inventories/{id}")
+    public ResponseEntity<Inventory> getInventorybyId(@PathVariable UUID id) {
+        Inventory inventory = inventoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Inventory not found with id " + id));
+        return ResponseEntity.ok(inventory);
     }
 
 }
