cca05c037989de1709039b3b9367148032d9f5ea
sathurshanath <sathurshanath2021@gmail.com>
2024-05-15 13:45:37 +0530

    Implement Admin APIs

---
+++ user-service/src/main/java/com/userservice/DTO/Response/Admin/AdminResponseMessage.java
@@ -0,0 +1,15 @@
+package com.userservice.DTO.Response.Admin;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AdminResponseMessage {
+
+    private String message;
+
+}
