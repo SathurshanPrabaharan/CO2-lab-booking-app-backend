a4b52729254391f1c2716c9be566afb122501396
sathurshanath <sathurshanath2021@gmail.com>
2024-05-18 14:18:05 +0530

    Implement Admin API Gateway

---
+++ admin-api-gateway/src/main/java/com/adminapigateway/AdminApiGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.adminapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApiGatewayApplication.class, args);
+    }
+}
