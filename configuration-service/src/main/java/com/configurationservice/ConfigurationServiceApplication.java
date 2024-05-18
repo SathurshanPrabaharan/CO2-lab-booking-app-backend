41057490f676dab57a13cb980ec50f96ead8d2c1
sathurshanath <sathurshanath2021@gmail.com>
2024-05-18 18:03:14 +0530

    Remove enable discovery client

--- configuration-service/src/main/java/com/configurationservice/ConfigurationServiceApplication.java
+++ configuration-service/src/main/java/com/configurationservice/ConfigurationServiceApplication.java
@@ -2,12 +2,9 @@ package com.configurationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ConfigurationServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigurationServiceApplication.class, args);
 	}
