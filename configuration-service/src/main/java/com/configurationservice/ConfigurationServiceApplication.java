bf3335372af68badc73c82241d9800567651b49a
sathurshanath <sathurshanath2021@gmail.com>
2024-05-20 06:19:41 +0530

    Implement the Authentication

--- configuration-service/src/main/java/com/configurationservice/ConfigurationServiceApplication.java
+++ configuration-service/src/main/java/com/configurationservice/ConfigurationServiceApplication.java
@@ -2,8 +2,10 @@ package com.configurationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigurationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigurationServiceApplication.class, args);
