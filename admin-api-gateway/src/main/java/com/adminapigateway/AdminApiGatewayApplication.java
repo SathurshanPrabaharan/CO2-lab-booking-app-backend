d309c666e03af46da7b91094dbdfba0264a93277
sathurshanath <sathurshanath2021@gmail.com>
2024-05-20 06:19:41 +0530

    Implement the Authentication

--- admin-api-gateway/src/main/java/com/adminapigateway/AdminApiGatewayApplication.java
+++ admin-api-gateway/src/main/java/com/adminapigateway/AdminApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.adminapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdminApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApiGatewayApplication.class, args);
