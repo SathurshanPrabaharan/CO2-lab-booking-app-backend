9453abf78b71cb65cbc8297ad272e1c11e4bcffe
sathurshanath <sathurshanath2021@gmail.com>
2024-05-24 15:07:37 +0530

    Add Repository annotation

--- user-service/src/main/java/com/userservice/Repositories/AdminRepository.java
+++ user-service/src/main/java/com/userservice/Repositories/AdminRepository.java
@@ -3,10 +3,12 @@ package com.userservice.Repositories;
 import com.userservice.Enums.STATUS;
 import com.userservice.Models.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface AdminRepository extends JpaRepository<Admin, UUID> {
     List<Admin> findByNameAndCreatedByAndStatus(String name, UUID createdBy, STATUS status);
 
--- user-service/src/main/java/com/userservice/Repositories/SupportRepositories/ProfessionRepository.java
+++ user-service/src/main/java/com/userservice/Repositories/SupportRepositories/ProfessionRepository.java
@@ -3,10 +3,12 @@ package com.userservice.Repositories.SupportRepositories;
 import com.userservice.Enums.STATUS;
 import com.userservice.Models.SupportModels.Profession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface ProfessionRepository extends JpaRepository<Profession, UUID> {
     List<Profession> findByNameAndCreatedByAndStatus(String name, UUID createdBy, STATUS status);
 
