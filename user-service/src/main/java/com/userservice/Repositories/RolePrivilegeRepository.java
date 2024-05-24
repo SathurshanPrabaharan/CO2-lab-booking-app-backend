5aa427e7ec2690b05a9dc1596fcf7a4ab0e1f33a
sathurshanath <sathurshanath2021@gmail.com>
2024-05-24 15:07:37 +0530

    Add Repository annotation

--- user-service/src/main/java/com/userservice/Repositories/RolePrivilegeRepository.java
+++ user-service/src/main/java/com/userservice/Repositories/RolePrivilegeRepository.java
@@ -2,14 +2,15 @@ package com.userservice.Repositories;
 
 
 import com.userservice.Enums.STATUS;
-import com.userservice.Models.Admin;
 import com.userservice.Models.RolePrivilege;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface RolePrivilegeRepository extends JpaRepository<RolePrivilege, UUID> {
 
     Optional<RolePrivilege> findByKey(String key);
@@ -19,4 +20,6 @@ public interface RolePrivilegeRepository extends JpaRepository<RolePrivilege, UU
     List<RolePrivilege> findByCreatedBy(UUID createdBy);
 
     List<RolePrivilege> findByStatus(STATUS status);
+
+    boolean existsByKey(String key);
 }
