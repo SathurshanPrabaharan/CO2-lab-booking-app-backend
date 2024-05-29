28aa6e8c0d5f0829cba15b5d6b67571f897f762a
darship19 <darshika197@gmail.com>
2024-05-29 21:58:59 +0530

    feat: update DTO response and inventoryService some feature

--- inventory-service/src/main/java/com/inventoryservice/Services/Impls/InventoryServiceImpl.java
+++ inventory-service/src/main/java/com/inventoryservice/Services/Impls/InventoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.inventoryservice.Services.Impls;
 
 import com.inventoryservice.DTO.Request.InventoryCreateRequest;
 import com.inventoryservice.DTO.Request.InventoryUpdateRequest;
+import com.inventoryservice.Enums.STATUS;
 import com.inventoryservice.Exception.InventoryNotFoundException;
 import com.inventoryservice.Models.Inventory;
 import com.inventoryservice.Repositories.InventoryRepository;
@@ -9,6 +10,7 @@ import com.inventoryservice.Services.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -18,22 +20,40 @@ public class InventoryServiceImpl implements InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Override
-    public Inventory saveInventory(InventoryCreateRequest inventoryRequest) {
-        Inventory inventory = Inventory.build(UUID.randomUUID(), inventoryRequest.getName(), inventoryRequest.getSerialNum(), inventoryRequest.getManufacturer(), inventoryRequest.getModel(),
-                inventoryRequest.getProcessor(), inventoryRequest.getMemoryType(), inventoryRequest.getMemorySize(), inventoryRequest.getStorageType(),
-                inventoryRequest.getStorageSize(), inventoryRequest.getOperatingSystem(), inventoryRequest.getStatus(), inventoryRequest.getPurchaseDate(),
-                inventoryRequest.getPurchaseCost(), inventoryRequest.getWarrantyExpiry(), inventoryRequest.getShortNote(), inventoryRequest.getLastMaintenanceDate(),
-                inventoryRequest.getNextMaintenanceDate(), inventoryRequest.getCreatedAt(), inventoryRequest.getUpdatedAt(), inventoryRequest.getCreatedBy(),
-                inventoryRequest.getInstalledSoftwares());
+    public Inventory saveInventory(InventoryCreateRequest inventoryCreateRequest) {
+        Inventory inventory = Inventory.builder()
+                .id(UUID.randomUUID())
+                .name(inventoryCreateRequest.getName())
+                .serialNum(inventoryCreateRequest.getSerialNum())
+                .manufacturer(inventoryCreateRequest.getManufacturer())
+                .model(inventoryCreateRequest.getModel())
+                .processor(inventoryCreateRequest.getProcessor())
+                .memoryType(inventoryCreateRequest.getMemoryType())
+                .memorySize(inventoryCreateRequest.getMemorySize())
+                .storageSize(inventoryCreateRequest.getStorageSize())
+                .operatingSystem(inventoryCreateRequest.getOperatingSystem())
+                .status(STATUS.ACTIVE)
+                .purchaseDate(inventoryCreateRequest.getPurchaseDate())
+                .purchaseCost(inventoryCreateRequest.getPurchaseCost())
+                .warrantyExpiry(inventoryCreateRequest.getWarrantyExpiry())
+                .shortNote(inventoryCreateRequest.getShortNote())
+                .lastMaintenanceDate(inventoryCreateRequest.getLastMaintenanceDate())
+                .nextMaintenanceDate(inventoryCreateRequest.getNextMaintenanceDate())
+                .createdAt(inventoryCreateRequest.getCreatedAt())
+                .updatedAt(inventoryCreateRequest.getUpdatedAt())
+                .createdBy(inventoryCreateRequest.getCreatedBy())
+                .installedSoftwares(inventoryCreateRequest.getInstalledSoftwares())
+                .build();
         return inventoryRepository.save(inventory);
     }
 
-    public Inventory getInventory(UUID id) throws InventoryNotFoundException {
-        Inventory inventory = inventoryRepository.findById(id)
-                .orElseThrow(() -> new InventoryNotFoundException("Inventory not found with ID: " + id));
-        return inventory;
 
+    @Override
+    public Inventory findById(UUID id) {
+        Optional<Inventory> AdminOptional = inventoryRepository.findById(id);
+        return AdminOptional.orElseThrow(() -> new InventoryNotFoundException("admin not found with id : " + id));
     }
+
 
     @Override
     public Inventory updateInventory(UUID id, InventoryUpdateRequest inventoryDetails) {
