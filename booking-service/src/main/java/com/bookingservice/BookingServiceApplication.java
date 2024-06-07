081bf436b868e8d2f9b1570c5bcea81e0874cc95
sathurshanath <sathurshanath2021@gmail.com>
2024-06-08 02:16:35 +0530

    Modify the DotEnv file configuration

--- booking-service/src/main/java/com/bookingservice/BookingServiceApplication.java
+++ booking-service/src/main/java/com/bookingservice/BookingServiceApplication.java
@@ -1,11 +1,22 @@
 package com.bookingservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookingServiceApplication {
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure()
+//				.directory("./booking-service")
+				.directory("./")
+				.filename(".env")
+				.load();
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(BookingServiceApplication.class, args);
 	}
 
--- configuration-service/src/main/java/com/configurationservice/ConfigurationServiceApplication.java
+++ configuration-service/src/main/java/com/configurationservice/ConfigurationServiceApplication.java
@@ -1,11 +1,23 @@
 package com.configurationservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConfigurationServiceApplication {
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure()
+//                .directory("./configuration-service")
+				.directory("./")
+				.filename(".env")
+				.load();
+
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(ConfigurationServiceApplication.class, args);
 	}
 
