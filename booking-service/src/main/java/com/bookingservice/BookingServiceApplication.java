37ad26cf2a098797e1389c22a66adbc5fd8ec9fd
sathurshanath <sathurshanath2021@gmail.com>
2024-06-12 03:47:28 +0530

    Enable Discovery client

--- booking-service/src/main/java/com/bookingservice/BookingServiceApplication.java
+++ booking-service/src/main/java/com/bookingservice/BookingServiceApplication.java
@@ -3,8 +3,10 @@ package com.bookingservice;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BookingServiceApplication {
 	public static void main(String[] args) {
 
--- inventory-service/src/main/java/com/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/inventoryservice/InventoryServiceApplication.java
@@ -3,9 +3,11 @@ package com.inventoryservice;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 	public static void main(String[] args) {
 
